cd630c3fb5fc01bfbc6b85e9f88526ce7ab3279b
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-05-20 00:30:23 +0300

    change api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiURL: 'https://api.apilayer.com/fixer/',
-  apiKey: 'd3ymFqdQErH9Y38AXmeXBnZ4u1fVN9tz',
+  apiKey: 'GSU9KbQEzDCiqlEHEFysPCCal3Zy4Nr2',
 };
